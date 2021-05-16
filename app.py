8c5e7a967e6b27f3b167459c6f33f4acaef5ae9e
Kurt De Jonghe <kurt.de.jonghe2@gmail.com>
2021-05-16 10:22:39 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    first_name = "Kurt"
+    stuff = "This is <strong>Bold</strong> Text"
+    favorit_pizza = ["Pepperoni", "Cheese", "Mushrooms", 50]
+    return render_template("index.html",
+                           first_name=first_name,
+                           stuff=stuff,
+                           favorit_pizza=favorit_pizza)
+
+
+@app.route("/user/<name>")
+def user(name):
+    return render_template("user.html", name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template("500.html"), 500
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
